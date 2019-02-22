31d9ed22d6b1ec73e54145910d6953888c319eff
Евгений Шевченко (work) <asyven3@gmail.com>
2019-02-22 13:41:31 +0200

    some updates

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,30 +10,25 @@ class ApiController extends Controller
 {
     public function proximity(Request $request)
     {
-//        if($request->isMethod('post') && $request->has('geo')){
-        $rq = $request->all();
+        if ($request->isMethod('post') && $request->has('geo')) {
+            $rq = $request->all();
 
-        $rq["geo"] = [
-            "lat" => "-43.1234",
-            "lng" => "-34.1234"
-        ];
+            $users = DB::table('users_address')
+                ->whereBetween('lat', [floor($rq["geo"]['lat']), ceil($rq["geo"]['lat'])])
+                ->whereBetween('lng', [floor($rq["geo"]['lng']), ceil($rq["geo"]['lng'])])
+                ->get();
 
-        $users = DB::table('users_address')
-            ->whereBetween('lat', [floor($rq["geo"]['lat']), ceil($rq["geo"]['lat'])])
-            ->whereBetween('lng', [floor($rq["geo"]['lng']), ceil($rq["geo"]['lng'])])
-            ->get();
+            $find_array = [];
+            foreach ($users as $user) {
+                $find_array[] = $user->id;
+            }
 
-        $find_array = [];
-        foreach ($users as $user) {
-            $find_array[] = $user->id;
+            $users_posts = DB::table('users_posts')
+                ->whereIn("customUser", $find_array)
+                ->get();
+
+
+            return response(["data" => $users_posts], 200);
         }
-
-        $users_posts = DB::table('users_posts')
-            ->whereIn("customUser", $find_array)
-            ->get();
-
-
-        return response(["data"=>$users_posts],200);
     }
-//    }
 }
--- database/seeds/CustomUsersSeeder.php
+++ database/seeds/CustomUsersSeeder.php
@@ -11,36 +11,12 @@ class CustomUsersSeeder extends Seeder
      * Run the database seeds.
      *
      * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function run()
     {
         $client = new GuzzleHttp\Client();
         $response = $client->request('GET', 'https://jsonplaceholder.typicode.com/users');
-/*
-        {
-        "id": 1,
-        "name": "Leanne Graham",
-        "username": "Bret",
-        "email": "Sincere@april.biz",
-        "address": {
-                "street": "Kulas Light",
-          "suite": "Apt. 556",
-          "city": "Gwenborough",
-          "zipcode": "92998-3874",
-          "geo": {
-                    "lat": "-37.3159",
-            "lng": "81.1496"
-          }
-        },
-        "phone": "1-770-736-8031 x56442",
-        "website": "hildegard.org",
-        "company": {
-                "name": "Romaguera-Crona",
-          "catchPhrase": "Multi-layered client-server neural-net",
-          "bs": "harness real-time e-markets"
-        }
-      },
-*/
 
        $custom_users =  json_decode($response->getBody()->getContents(),true);
 
